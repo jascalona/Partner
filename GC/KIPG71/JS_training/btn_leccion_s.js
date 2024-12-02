755e724fd28850a189c5d181999cf073a1b18c46
unknown <JEscalona@tESTHG.lan.xven.com.ve>
2024-12-02 18:57:48 -0400

    se finalizo el modulo de software

--- GC/KIPG71/JS_training/btn_leccion_s.js
+++ GC/KIPG71/JS_training/btn_leccion_s.js
@@ -13,6 +13,9 @@ function ir_leccion1_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion1').style.display = 'block';
 }  
 
@@ -29,6 +32,9 @@ function ir_leccion2_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion2').style.display = 'block';
 }    
 
@@ -45,6 +51,9 @@ function ir_leccion3_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion3').style.display = 'block';
 }    
 
@@ -61,6 +70,9 @@ function ir_leccion4_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion4').style.display = 'block';
 }    
 
@@ -77,6 +89,9 @@ function ir_leccion5_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion5').style.display = 'block';
 }  
 
@@ -93,6 +108,9 @@ function ir_leccion6_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion6').style.display = 'block';
 }  
 
@@ -109,6 +127,9 @@ function ir_leccion7_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion7').style.display = 'block';
 }  
 
@@ -126,6 +147,9 @@ function ir_leccion8_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion8').style.display = 'block';
 }
 
@@ -142,6 +166,9 @@ function ir_leccion9_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion9').style.display = 'block';
 }
 
@@ -158,6 +185,9 @@ function ir_leccion10_s(){
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion10').style.display = 'block';
 }
 
@@ -174,6 +204,9 @@ function ir_leccion11_s(){
     document.getElementById('leccion10').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion11').style.display = 'block';
 }
 
@@ -190,6 +223,9 @@ function ir_leccion12_s(){
     document.getElementById('leccion10').style.display = 'none';
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion12').style.display = 'block';
 }
 
@@ -206,5 +242,67 @@ function ir_leccion13_s(){
     document.getElementById('leccion10').style.display = 'none';
     document.getElementById('leccion11').style.display = 'none';
     document.getElementById('leccion12').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
     document.getElementById('leccion13').style.display = 'block';
+}
+
+function ir_leccion14_s(){
+    document.getElementById('leccion1').style.display = 'none';
+    document.getElementById('leccion2').style.display = 'none';
+    document.getElementById('leccion3').style.display = 'none';
+    document.getElementById('leccion4').style.display = 'none';
+    document.getElementById('leccion5').style.display = 'none';
+    document.getElementById('leccion6').style.display = 'none';
+    document.getElementById('leccion7').style.display = 'none';
+    document.getElementById('leccion8').style.display = 'none';
+    document.getElementById('leccion9').style.display = 'none';
+    document.getElementById('leccion10').style.display = 'none';
+    document.getElementById('leccion11').style.display = 'none';
+    document.getElementById('leccion12').style.display = 'none';
+    document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'block';
+}
+
+function ir_leccion15_s(){
+    document.getElementById('leccion1').style.display = 'none';
+    document.getElementById('leccion2').style.display = 'none';
+    document.getElementById('leccion3').style.display = 'none';
+    document.getElementById('leccion4').style.display = 'none';
+    document.getElementById('leccion5').style.display = 'none';
+    document.getElementById('leccion6').style.display = 'none';
+    document.getElementById('leccion7').style.display = 'none';
+    document.getElementById('leccion8').style.display = 'none';
+    document.getElementById('leccion9').style.display = 'none';
+    document.getElementById('leccion10').style.display = 'none';
+    document.getElementById('leccion11').style.display = 'none';
+    document.getElementById('leccion12').style.display = 'none';
+    document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'block';
+
+}
+
+function ir_leccion16_s(){
+    document.getElementById('leccion1').style.display = 'none';
+    document.getElementById('leccion2').style.display = 'none';
+    document.getElementById('leccion3').style.display = 'none';
+    document.getElementById('leccion4').style.display = 'none';
+    document.getElementById('leccion5').style.display = 'none';
+    document.getElementById('leccion6').style.display = 'none';
+    document.getElementById('leccion7').style.display = 'none';
+    document.getElementById('leccion8').style.display = 'none';
+    document.getElementById('leccion9').style.display = 'none';
+    document.getElementById('leccion10').style.display = 'none';
+    document.getElementById('leccion11').style.display = 'none';
+    document.getElementById('leccion12').style.display = 'none';
+    document.getElementById('leccion13').style.display = 'none';
+    document.getElementById('leccion14').style.display = 'none';
+    document.getElementById('leccion15').style.display = 'none';
+    document.getElementById('leccion16').style.display = 'block';
+
 }
