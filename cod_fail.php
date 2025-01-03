1852b03b44a5a1d876561531a648a04abc6da17f
Jose Escalona <JEscalona@lan.xven.com.ve>
2025-01-03 12:14:55 -0400

    se agrego un offcanva para la calificacion

--- cod_fail.php
+++ cod_fail.php
@@ -53,68 +53,154 @@ if (!empty($_SESSION["name"])) {
     </ul>
   </header>
 
-        
+
 
   <main class="table-fail">
 
-  <!-- start search filter -->
+    <!-- start search filter -->
     <div class="search" data-aos="fade-up" data-aos-duration="1000">
-        <div class="container">
-            <form class="">
-                <input class="form-control me-2 light-table-filter" data-table="table" type="text" placeholder="Buscar...">
-            </form>
-        </div>
+      <div class="container">
+        <form class="">
+          <input class="form-control me-2 light-table-filter" data-table="table" type="text" placeholder="Buscar...">
+        </form>
+      </div>
     </div>
     <!-- end search filter -->
 
     <div class="icons">
 
-        <div class="icon-item">
-            <a href=""><i class='bx bxs-cog' style='color:#5e5a5a'></i></a><br>
-            <a href=""><small>Plataformas</small></a>
-        </div>
+      <div class="icon-item">
+        <a href=""><i class='bx bxs-cog' style='color:#5e5a5a'></i></a><br>
+        <a href=""><small>Plataformas</small></a>
+      </div>
 
-        <div class="icon-item">
-            <a href=""><i class='bx bxl-microsoft' style='color:#5e5a5a'  ></i></a><br>
-            <a href=""><small>Fallas Mecánica</small></a>
-        </div>
+      <div class="icon-item">
+        <a href=""><i class='bx bxl-microsoft' style='color:#5e5a5a'></i></a><br>
+        <a href=""><small>Fallas Mecánica</small></a>
+      </div>
 
     </div>
 
     <!--Table-->
 
-    <?php include'./CONTROLLER/conexion.php' ?>
+    <?php include './CONTROLLER/conexion.php' ?>
 
     <table class="table">
 
-        <thead class="table table-dark">
-          <tr>
-            <th scope="col">Modelo</th>
-            <th scope="col">Codigo</th>
-          </tr>
-        </thead>
+      <thead class="table table-dark">
+        <tr>
+          <th scope="col">Modelo</th>
+          <th scope="col">Codigo</th>
+          <th scope="col">Solucion</th>
+        </tr>
+      </thead>
 
 
-        <!--Sentencia de seleccion-->
-        <?php  
-        $sql = $conexion->query('SELECT *FROM cod_fail');
-        while ($datos = $sql->fetch_object()) {
+      <!--Sentencia de seleccion-->
+      <?php
+      $sql = $conexion->query('SELECT *FROM cod_fail');
+      while ($datos = $sql->fetch_object()) {
         ?>
         <!--Sentencia de seleccion-->
 
 
         <tbody>
           <tr>
-            <td scope="row"><?= $datos->modelo?></th>
-            <td scope="row"><?= $datos->codigoF?></th>
+            <td scope="row"><?= $datos->modelo ?></th>
+            <td scope="row"><?= $datos->codigoF ?></th>
+
+            <td>
+              <!--Ventana description-->
+              <div class="container-modal-desc">
+                <h6>Como abordar el error <i style="color: red;"><?php echo $datos->codigoF ?></i></h6>
+                <p><small>
+                    <?php echo $datos->desc ?>
+                  </small></p>
+
+                <div class="btn-calification">
+                  <button class="btn btn-dark" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight"
+                    aria-controls="offcanvasRight" >
+                    <i class='bx bxs-comment-detail' style='color:#f3f3f3f3'></i>
+                  </button>
+
+                  <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight"
+                    aria-labelledby="offcanvasRightLabel">
+                    <div class="offcanvas-header">
+                      <h5 class="offcanvas-title" id="offcanvasRightLabel">¿Como Calificarías esta solución?</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                    </div>
+                    <div class="offcanvas-body">
+
+                      <p><small>
+                        Error <?php echo $datos->codigoF?>
+                      </small></p>
+
+                      <br>
+
+                    <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="excelente">
+                        <label class="form-check-label" for="excelente">
+                          Excelente, fue muy útil esta información
+                        </label>
+                      </div>
+
+                      <br>
+
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="impreciso">
+                        <label class="form-check-label" for="impreciso">
+                          Impreciso: no conincide con lo que veo en el producto
+                        </label>
+                      </div>
+
+                      <br>
+                      
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="dificil"
+                          checked>
+                        <label class="form-check-label" for="dificil">
+                          Difícil de entender: traducción poco clara o incorrecta
+                        </label>
+                      </div>
+
+                      <br>
+
+                      <div class="form-check">
+                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="falta"
+                          checked>
+                        <label class="form-check-label" for="falta">
+                          Falta información: es relevante pero no está completa
+                        </label>
+                      </div>
+
+                      <br>
+
+                      <div class="comentario-adicional">
+                        <h6>Compartir información o sugerencias adicionales</h6>
+                        <textarea class="form-control" name="message" id="message" placeholder="Ingrese un Comentario..."></textarea>
+                      
+                        <button class="btn btn-dark">Enviar</button>
+                      </div>
+          
+                    </div>
+                  </div>
+
+                </div>
+                <!--Ventana Modal-->
+
+
+
+            </td>
+
+
           </tr>
         </tbody>
 
         <?php
-        }
-        ?>
+      }
+      ?>
 
-      </table>
+    </table>
     <!--Table-->
 
-</main>
+  </main>
